90f1fa5d20bb799ffaa998f59ff55e57f8f9c0d7
吴眉 <wumei@wumeideMacBook-Pro.local>
2017-11-02 12:16:40 +0400

    committing first poem

---
+++ individuals/mei/poem 1.py
@@ -0,0 +1,17 @@
+import random
+import time
+names=["Mei","She","He","It"]
+place=["maze", "river","sky", "ocean","castle","physics class"]
+verb=["play", "sleep", "drink", "sing","tear","throw", "swallow"]
+thing=["poem", "dream", "xiao long bao", "langrande equation","cloud","python"]
+
+while 1:
+    conclusion=random.random()
+    
+    if conclusion>0.9:
+        print ("The"+"%s"%(random.choice(thing)+" is "+"%s"%(random.choice(verb)))+"ing")
+    
+    print ("%s"%(random.choice(names))+" stays in the "+"%s"%(random.choice(place)))
+    print ("%s"%(random.choice(verb))+"ing "+"%s"%(random.choice(thing)))
+    time.sleep(1)
+    
