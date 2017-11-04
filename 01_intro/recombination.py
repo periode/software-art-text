3aab1c33b614922a2608f9aee1c4f485387c9baa
Pierre Depaz <pierre.depaz@gmail.com>
2017-11-04 14:47:12 +0400

    modified recombination.py

--- 01_intro/recombination.py
+++ 01_intro/recombination.py
@@ -8,16 +8,29 @@ act = ['jumps', 'caresses', 'envelops', 'touches', 'reaches into', 'pets']
 victim_adjectives = ['frightened', 'foolish', 'eager', 'loud', 'quiet', 'unconscious', 'wide-eyed']
 victim = ['shadow', 'shiver', 'glimpse', 'crawl', 'sinner', 'saviour', 'hand', 'smile', 'cry']
 
+def pickRandomNumber():
+    return random.random()
+
 while True:
     print "the %s %s %s the %s." % (random.choice(actor_adjectives), random.choice(actor), random.choice(act), random.choice(victim_adjectives)),
-    conclusion = random.random()
-    if conclusion > 0.8:
+
+    conclusion = pickRandomNumber()
+
+    if conclusion > 0.85:
+        print ""
+        sleep(1)
+        print ""
+        sleep(1)
         print "there is a %s." % random.choice(victim)
         exit()
-    elif conclusion > 0.6:
+    elif conclusion > 0.7:
+        print ""
+        sleep(1)
+        print ""
+        sleep(1)
         print "there is no %s." % random.choice(victim)
         exit()
-    else:
-        print ""
+
     sys.stdout.flush()
+
     sleep(3)
