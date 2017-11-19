eb84e841294179c1ee8d39bcfe876c39d4a60ac6
Pierre Depaz <pierre.depaz@gmail.com>
2017-11-19 16:05:10 +0400

    changed line 2

--- 03_language/e_wordnik.py
+++ 03_language/e_wordnik.py
@@ -2,7 +2,7 @@ from wordnik import *
 
 # here we set up all we need to get access to the Wordnik API
 api_url = 'http://api.wordnik.com/v4'
-api_key = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+api_key = 'dc73bd2b889304d23700403b0de048a90e8f5f60a0e42d0f1'
 client = swagger.ApiClient(api_key, api_url)
 
 print ""
